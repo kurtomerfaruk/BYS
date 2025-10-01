178eb5f3a3e0c34bb08fecc0952d3164dc734f60
Ömer Faruk Kurt <kurtomerfaruk@gmail.com>
2025-10-01 09:12:20 +0300

    Servis eklendi

---
+++ BilgiYonetimSistemi/src/main/java/tr/bel/gaziantep/bysweb/moduls/engelsizler/service/EyKisiEngelGrubuService.java
@@ -0,0 +1,34 @@
+package tr.bel.gaziantep.bysweb.moduls.engelsizler.service;
+
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import tr.bel.gaziantep.bysweb.core.service.AbstractService;
+import tr.bel.gaziantep.bysweb.core.utils.Constants;
+import tr.bel.gaziantep.bysweb.moduls.engelsizler.entity.EyKisiEngelGrubu;
+
+import java.io.Serial;
+
+/**
+ * @author Omer Faruk KURT kurtomerfaruk@gmail.com
+ * @version 1.0.0
+ * @since 1.10.2025 09:11
+ */
+@Stateless
+public class EyKisiEngelGrubuService extends AbstractService<EyKisiEngelGrubu> {
+
+    @Serial
+    private static final long serialVersionUID = -7013186229396466049L;
+
+    public EyKisiEngelGrubuService() {
+        super(EyKisiEngelGrubu.class);
+    }
+
+    @PersistenceContext(unitName = Constants.UNIT_NAME)
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+}
