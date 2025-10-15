240356a968e9e2ebc2b1761cff6a030e7d2eae10
Ömer Faruk Kurt <kurtomerfaruk@gmail.com>
2025-10-15 09:43:49 +0300

    fiyat guncelleme icin metot eklendi

--- BilgiYonetimSistemi/src/main/java/tr/bel/gaziantep/bysweb/moduls/ortezprotez/controller/OrtStokController.java
+++ BilgiYonetimSistemi/src/main/java/tr/bel/gaziantep/bysweb/moduls/ortezprotez/controller/OrtStokController.java
@@ -4,15 +4,24 @@ import jakarta.faces.model.SelectItem;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.primefaces.PrimeFaces;
 import org.primefaces.event.SelectEvent;
 import tr.bel.gaziantep.bysweb.core.controller.AbstractController;
+import tr.bel.gaziantep.bysweb.core.enums.ErrorType;
+import tr.bel.gaziantep.bysweb.core.enums.bys.EnumAlisSatis;
 import tr.bel.gaziantep.bysweb.core.enums.sistemyonetimi.EnumSyFiltreAnahtari;
+import tr.bel.gaziantep.bysweb.core.exception.BysBusinessException;
 import tr.bel.gaziantep.bysweb.core.service.FilterOptionService;
+import tr.bel.gaziantep.bysweb.core.utils.Constants;
+import tr.bel.gaziantep.bysweb.core.utils.FacesUtil;
 import tr.bel.gaziantep.bysweb.moduls.ortezprotez.entity.OrtStok;
+import tr.bel.gaziantep.bysweb.moduls.ortezprotez.service.OrtStokService;
 
 import java.io.Serial;
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 
@@ -30,7 +39,16 @@ public class OrtStokController extends AbstractController<OrtStok> {
     private static final long serialVersionUID = -3373843224409827700L;
 
     @Inject
+    private OrtStokService service;
+    @Inject
     private FilterOptionService filterOptionService;
+
+    @Getter
+    @Setter
+    private EnumAlisSatis fiyatTur;
+    @Getter
+    @Setter
+    private BigDecimal fiyat;
 
     public OrtStokController() {
         super(OrtStok.class);
@@ -54,5 +72,23 @@ public class OrtStokController extends AbstractController<OrtStok> {
     public void onRowDblSelect(SelectEvent<OrtStok> event) {
         OrtStok ortStok = event.getObject();
         stokSecKapat(ortStok);
+    }
+
+    public void preparePrice(){
+        if (this.getSelected() == null) {
+            throw new BysBusinessException(ErrorType.NESNE_OKUNAMADI);
+        }
+        fiyat = BigDecimal.ZERO;
+        fiyatTur = EnumAlisSatis.SATIS;
+    }
+
+    public void updatePrice(){
+        try {
+            service.updatePrice(this.getSelected(),fiyatTur,fiyat);
+            FacesUtil.successMessage("fiyatGuncellendi");
+        }catch (Exception ex) {
+            log.error(null, ex);
+            FacesUtil.errorMessage(Constants.HATA_OLUSTU);
+        }
     }
 }
